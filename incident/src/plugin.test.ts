d6851074f80bbbfb2501aa3e3b62293105345b2d
Sam Starling <42478+samstarling@users.noreply.github.com>
2023-06-23 14:23:24 +0100

    Fix issue with incorrect import

--- incident/src/plugin.test.ts
+++ incident/src/plugin.test.ts
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { incidentIoPlugin } from './plugin';
+import { incidentPlugin } from "./plugin";
 
-describe('incident', () => {
-  it('should export plugin', () => {
-    expect(incidentIoPlugin).toBeDefined();
+describe("incident", () => {
+  it("should export plugin", () => {
+    expect(incidentPlugin).toBeDefined();
   });
 });
